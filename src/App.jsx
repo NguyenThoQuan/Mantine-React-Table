48947a5a07aa96b3cff022cb3f795b2894a9bec0
NguyenThoQuan <thoquan150903@gmail.com>
2024-06-11 14:21:35 +0700

    Export Excel

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useMemo } from 'react';
-import './App.css'
+import './App.css';
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 import 'mantine-react-table/styles.css';
 import { useMantineReactTable, MantineReactTable } from 'mantine-react-table';
-import { ActionIcon, Box } from '@mantine/core';
-import { IconEdit, IconCopy, IconTrash } from '@tabler/icons-react';
+import { ActionIcon, Box, Button, Group } from '@mantine/core';
+import { IconEdit, IconCopy, IconTrash, IconDownload } from '@tabler/icons-react';
+import { mkConfig, generateCsv, download } from 'export-to-csv';
 import Data from "./data.json";
 import Header from './Header';
 import Nav from './Nav';
 
-console.log(Data)
+console.log(Data);
 
 function App() {
   const columns = useMemo(() => [
@@ -58,10 +59,30 @@ function App() {
     },
   ], []);
 
+  const csvConfig = mkConfig({
+    fieldSeparator: ',',
+    decimalSeparator: '.',
+    useKeysAsHeaders: true,
+  });
+
+  const handleExportData = () => {
+    const csv = generateCsv(csvConfig)(Data);
+    download(csvConfig)(csv);
+  };
+
   const tableData = useMantineReactTable({
     columns: columns,
     data: Data,
     enableRowSelection: true,
+    renderTopToolbarCustomActions: ({ table }) => (
+      <Group position="apart" style={{ width: '100%' }}>
+        <Button onClick={handleExportData}
+          leftIcon={<IconDownload />}
+          variant="filled" color='#fff' style={{ color: "#e14bd2", display: "flex", alignItems: "center", gap: "10px", border: "1px solid #e14bd2" }}>
+          <IconDownload />Export Excel
+        </Button>
+      </Group>
+    )
   });
 
   return (
@@ -76,7 +97,7 @@ function App() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
